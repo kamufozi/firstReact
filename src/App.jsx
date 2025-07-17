2fdc6e38dbcca52181a00784eb763dedacf415be
Fozi Chris <kamufozi679@gmail.com>
2025-07-17 12:55:40 +0200

    added mount fuji instance in scrimba class

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import Entry from './components/Entry'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Entry />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 export default function Header(){
     return(
-    <header className=' flex justify-center items-center bg-pink-600 gap-2.5 text-white p-5' >
+    <header className=' flex justify-center items-center bg-[#F55A5A] gap-2.5 text-white p-5' >
         <FontAwesomeIcon icon={faGlobe} />
         <p>my travel journal.</p>
     </header>
